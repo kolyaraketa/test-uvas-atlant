cdb3fdc997440020a9ead9f6bad8c755adb6d954
Kolya Raketa <raketakolya@gmail.com>
2020-09-16 12:50:49 +0300

    publish gh pages

---
+++ precache-manifest.2b1945cd43b935d91f8e248d579c1629.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd0be1345dab1270a15",
+    "url": "/test-uvas-atlant/css/app.f47831aa.css"
+  },
+  {
+    "revision": "c13dd21ee3284aefe65e",
+    "url": "/test-uvas-atlant/css/chunk-19e4e008.14848a0e.css"
+  },
+  {
+    "revision": "b7fe5a7f469daa6595b7",
+    "url": "/test-uvas-atlant/css/chunk-76a26905.7b430eab.css"
+  },
+  {
+    "revision": "ba7840ef9a0d6720945b",
+    "url": "/test-uvas-atlant/css/chunk-vendors.80aba39f.css"
+  },
+  {
+    "revision": "240d08ad61486b3c97e75b8d6b45429d",
+    "url": "/test-uvas-atlant/index.html"
+  },
+  {
+    "revision": "6bd0be1345dab1270a15",
+    "url": "/test-uvas-atlant/js/app.04b1a474.js"
+  },
+  {
+    "revision": "c13dd21ee3284aefe65e",
+    "url": "/test-uvas-atlant/js/chunk-19e4e008.82c305d0.js"
+  },
+  {
+    "revision": "b7fe5a7f469daa6595b7",
+    "url": "/test-uvas-atlant/js/chunk-76a26905.f4475e8a.js"
+  },
+  {
+    "revision": "ba7840ef9a0d6720945b",
+    "url": "/test-uvas-atlant/js/chunk-vendors.b8536ce0.js"
+  },
+  {
+    "revision": "9f2d60998b1fa1622728737f12585a63",
+    "url": "/test-uvas-atlant/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-uvas-atlant/robots.txt"
+  }
+]);
--- src/helper/defaults.js
+++
@@ -1,6 +0,0 @@
-export const TILE = {
-  COUNT: 5,
-  WIDTH: 300,
-  HEIGHT: 100,
-  PADDING: 30
-};
