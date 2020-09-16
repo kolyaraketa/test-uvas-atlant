b888b7a7f5c0078afd2203af981926476f910995
Kolya Raketa <raketakolya@gmail.com>
2020-09-16 12:34:39 +0300

    Added GH Pages

---
+++ docs/precache-manifest.661a4d8ccc3ed50896de97a5db53e35c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc9a316071003d99ef93",
+    "url": "/css/app.f47831aa.css"
+  },
+  {
+    "revision": "46125d4475d0008efc3a",
+    "url": "/css/chunk-19e4e008.14848a0e.css"
+  },
+  {
+    "revision": "b266b5269c4971193754",
+    "url": "/css/chunk-76a26905.7b430eab.css"
+  },
+  {
+    "revision": "ba7840ef9a0d6720945b",
+    "url": "/css/chunk-vendors.80aba39f.css"
+  },
+  {
+    "revision": "79744d9e013b4143c86b30bf9464035f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9a316071003d99ef93",
+    "url": "/js/app.a5a509d6.js"
+  },
+  {
+    "revision": "46125d4475d0008efc3a",
+    "url": "/js/chunk-19e4e008.2b873dce.js"
+  },
+  {
+    "revision": "b266b5269c4971193754",
+    "url": "/js/chunk-76a26905.1a46066f.js"
+  },
+  {
+    "revision": "ba7840ef9a0d6720945b",
+    "url": "/js/chunk-vendors.b8536ce0.js"
+  },
+  {
+    "revision": "9f2d60998b1fa1622728737f12585a63",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
